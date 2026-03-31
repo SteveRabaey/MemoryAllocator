b2904c538698e7feb65e58b08d264cbddaccb5ee
SteveRabaey <steve2002@live.be>
2026-04-01 01:09:34 +0200

    refactoring some logic and implementing free function

--- program/allocator.c
+++ program/allocator.c
@@ -16,12 +16,17 @@ typedef struct Block
     // 8 bytes on a 64bit OS
     size_t size;
     int free;
+    //next free block
     struct Block *next;
+
+    //prev block on the heap
+    struct Block *prev
 }Block;
 
-static Block *free_header_list = NULL; 
+static Block *free_allocator_list = NULL; 
 
 char* my_alloc(int size);
+void free_mem(char *or_ntr);
 int main()
 {
    
@@ -29,9 +34,11 @@ int main()
     char *ptr = my_alloc(16);
     char *ptr2 = my_alloc(16);
 
-
+   
     *ptr = 'E';
     *ptr2 = 'B';
+
+     free_mem(ptr);
     printf("%c%c\n", *ptr, *ptr2);
 
     return 0;
@@ -53,24 +60,64 @@ char*  my_alloc(int size)
         start_memory = memory;
         end_memory = memory + (sizeof(size_t) * 200);
 
-        printf("%p\n", start_memory);
-        printf("%p\n", end_memory);
-
-        free_header_list = memory;
+        free_allocator_list = memory + sizeof(size_t);
+        
 
     }
-    char *return_address = (char*)free_header_list + sizeof(Block);
-    free_header_list->size = size;
-    free_header_list->free = 0;
-
+   
+    char *return_address = NULL;
     if(start_memory < end_memory)
     {
-
-        Block *next = (Block*)((char*)free_header_list + sizeof(Block) + size);
-        // free_header_list->next = next;
+        // check size
+        //todo: need to implement prev
+        if((end_memory - start_memory) >= (size + sizeof(Block)))
+        {
+            return_address = (char*)free_allocator_list;
+            free_allocator_list->size = size;
+            free_allocator_list->free = 0;
+        }else
+        {
+            // need to check the freelist for free spots that are big enough
+            //set this in a method to reuse under this if. also if no size is big enough request now space.
+            //allocateFromList()
+        }
+        
+        Block *next = (Block*)((char*)free_allocator_list + sizeof(Block) + size);
+        // free_allocator_list->next = next;
         start_memory = (char*)next;
-        free_header_list = next;
+        next->prev = free_allocator_list;
+        free_allocator_list = next;
         
     }
+    // allocateFromList()
     return return_address;
+}
+//allocateFromList() check linked list for free
+void free_mem(char *or_ptr)
+{
+    Block *or_block = or_ptr - sizeof(Block);
+    // look for left neightboor + also update header
+    while(1)
+    {
+        //Search pointer of the left header
+        Block *left_block = or_block->prev - sizeof(Block);
+        //segmentation fault ????
+        if(left_block->free == 1)
+        {
+            left_block->size += (or_block->size + sizeof(Block));
+            or_block = left_block;
+        }else
+        {
+            or_block->free = 1;
+            return 0;
+        }
+
+    }
+
+    //search riht neighboor if flag is free merge. loop this.
+    //dont forget to update linked list every time
+
+ 
+
+    //set the header on free
 }
